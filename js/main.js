e5ba7770cd08e8a663e0a525d85ae0816e96264a
andrew <arenn@design.upenn.edu>
2021-05-01 08:54:15 -0400

    Test launch

--- js/main.js
+++ js/main.js
@@ -9,26 +9,121 @@ $(document).ready(function(){
     });
 });
 
-// The svg
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+Promise.all([
+  d3.json('https://raw.githubusercontent.com/asrenninger/gravity/main/data/blocks.json'),
+  d3.json('https://raw.githubusercontent.com/asrenninger/networks/master/data/processed/background.geojson')
+]).then(([blocks, background]) =>  {
+  console.log(blocks)
+  console.log(background)
 
-// Load external data and boot
-d3.json("https://raw.githubusercontent.com/asrenninger/networks/master/data/processed/background.geojson").then(function(data){
+  var width = 800
+  var height = 800
+
+  // The svg
+  var svg = d3.select("#base")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+
 
   projection = d3.geoMercator()
-    .fitSize([width, height], data)
+    .fitSize([width, height], background)
+
+  path = d3.geoPath(projection)
 
     // Draw the map
     svg.append("g")
         .selectAll("path")
-        .data(data.features)
+        .data(background.features)
         .enter()
         .append("path")
           .attr("fill", "#000")
-          .attr("d", d3.geoPath()
-              .projection(projection)
-          )
+          .attr("d", path)
         .style("stroke", "none")
+
+    svg.append("g")
+       .selectAll("path")
+       .data(topojson.feature(blocks, blocks.objects.blocks).features)
+       .join("path")
+        .attr("class", "blockfills")
+        .attr("fill", "transparent")
+        .attr("d", path);
+
+    svg.append("path")
+        .datum(topojson.mesh(blocks, blocks.objects.blocks, (a, b) => a !== b))
+        .attr("class", "blocklines")
+        .attr("fill", "none")
+        .attr("stroke", "#fff")
+        .attr("stroke-width", 0.5)
+        .attr("stroke-dasharray", 2)
+        .attr("d", path);
+
+    legend({
+  color: d3.scaleThreshold()
+        .domain([2, 4, 8, 16, 32, 64, 128, 256])
+        .range(['#8C0172',
+                '#922D55',
+                '#964D3E',
+                '#9A6E28',
+                '#9B951B',
+                '#89BC48',
+                '#6BD48C',
+                '#66E8D3',
+                '#B2F2FD']),
+  title: "New visits"
 })
+
+});
+
+  // d3.json("https://raw.githubusercontent.com/asrenninger/networks/master/data/processed/background.geojson").then(function(data){
+  //
+  //   var width = 800
+  //   var height = 800
+  //
+  //   // The svg
+  //   var svg = d3.select("#base")
+  //     .append("svg")
+  //     .attr("width", width)
+  //     .attr("height", height)
+  //
+  //
+  //   projection = d3.geoMercator()
+  //     .fitSize([width, height], data)
+  //
+  //     // Draw the map
+  //     svg.append("g")
+  //         .selectAll("path")
+  //         .data(data.features)
+  //         .enter()
+  //         .append("path")
+  //           .attr("fill", "#000")
+  //           .attr("d", d3.geoPath()
+  //               .projection(projection)
+  //           )
+  //         .style("stroke", "none")
+  // })
+
+
+// // The svg
+// var svg = d3.select("svg"),
+//     width = +svg.attr("width"),
+//     height = +svg.attr("height");
+//
+// // Load external data and boot
+// d3.json("https://raw.githubusercontent.com/asrenninger/networks/master/data/processed/background.geojson").then(function(data){
+//
+//   projection = d3.geoMercator()
+//     .fitSize([width, height], data)
+//
+//     // Draw the map
+//     svg.append("g")
+//         .selectAll("path")
+//         .data(data.features)
+//         .enter()
+//         .append("path")
+//           .attr("fill", "#000")
+//           .attr("d", d3.geoPath()
+//               .projection(projection)
+//           )
+//         .style("stroke", "none")
+// })
